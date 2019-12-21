02cd5be14dcdc47a43130217a45655de3a693614
itgjz <itgjz@qq.com>
2019-12-21 12:22:41 +0800

    新增按钮可使用自定义文字

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import SignaturePad from "./src/index";
 
 ReactDOM.render(
-  <SignaturePad clearButton="true" />,
+  <SignaturePad
+    clearButton="true"
+    clearTxt="清空"
+  />,
   document.getElementById('container')
 )
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ export default class SignaturePad extends React.Component {
     this.minWidth = this.props.minWidth || 0.5;
     this.maxWidth = this.props.maxWidth || 2.5;
     this.dotSize = this.props.dotSize || function () {
-        return (this.minWidth + this.maxWidth) / 2;
+      return (this.minWidth + this.maxWidth) / 2;
     };
     this.penColor = this.props.penColor || "black";
     this.backgroundColor = this.props.backgroundColor || "rgba(0,0,0,0)";
@@ -34,11 +34,11 @@ export default class SignaturePad extends React.Component {
   }
 
   clear(e) {
-    if(e) {
+    if (e) {
       e.preventDefault();
     }
     var ctx = this._ctx,
-        canvas = this._canvas;
+      canvas = this._canvas;
 
     ctx.fillStyle = this.backgroundColor;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -53,10 +53,10 @@ export default class SignaturePad extends React.Component {
 
   fromDataURL(dataUrl) {
     var self = this,
-        image = new Image(),
-        ratio = window.devicePixelRatio || 1,
-        width = this._canvas.width / ratio,
-        height = this._canvas.height / ratio;
+      image = new Image(),
+      ratio = window.devicePixelRatio || 1,
+      width = this._canvas.width / ratio,
+      height = this._canvas.height / ratio;
 
     this._reset();
     image.src = dataUrl;
@@ -72,11 +72,11 @@ export default class SignaturePad extends React.Component {
 
   _resizeCanvas() {
     var ctx = this._ctx,
-        canvas = this._canvas;
+      canvas = this._canvas;
     // When zoomed out to less than 100%, for some very strange reason,
     // some browsers report devicePixelRatio as less than 1
     // and only part of the canvas is cleared then.
-    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+    var ratio = Math.max(window.devicePixelRatio || 1, 1);
     canvas.width = canvas.offsetWidth * ratio;
     canvas.height = canvas.offsetHeight * ratio;
 
@@ -136,30 +136,30 @@ export default class SignaturePad extends React.Component {
   };
 
   _handleMouseUp(event) {
-      if (event.which === 1 && this._mouseButtonDown) {
-          this._mouseButtonDown = false;
-          this._strokeEnd(event);
-      }
+    if (event.which === 1 && this._mouseButtonDown) {
+      this._mouseButtonDown = false;
+      this._strokeEnd(event);
+    }
   };
 
   _handleTouchStart(event) {
-      var touch = event.changedTouches[0];
-      this._strokeBegin(touch);
+    var touch = event.changedTouches[0];
+    this._strokeBegin(touch);
   };
 
   _handleTouchMove(event) {
-      // Prevent scrolling.
-      event.preventDefault();
+    // Prevent scrolling.
+    event.preventDefault();
 
-      var touch = event.changedTouches[0];
-      this._strokeUpdate(touch);
+    var touch = event.changedTouches[0];
+    this._strokeUpdate(touch);
   };
 
   _handleTouchEnd(event) {
-      var wasCanvasTouched = event.target === this._canvas;
-      if (wasCanvasTouched) {
-          this._strokeEnd(event);
-      }
+    var wasCanvasTouched = event.target === this._canvas;
+    if (wasCanvasTouched) {
+      this._strokeEnd(event);
+    }
   };
 
   _strokeUpdate(event) {
@@ -177,7 +177,7 @@ export default class SignaturePad extends React.Component {
 
   _strokeDraw(point) {
     var ctx = this._ctx,
-        dotSize = typeof(this.dotSize) === 'function' ? this.dotSize() : this.dotSize;
+      dotSize = typeof (this.dotSize) === 'function' ? this.dotSize() : this.dotSize;
 
     ctx.beginPath();
     this._drawPoint(point.x, point.y, dotSize);
@@ -187,7 +187,7 @@ export default class SignaturePad extends React.Component {
 
   _strokeEnd(event) {
     var canDrawCurve = this.points.length > 2,
-        point = this.points[0];
+      point = this.points[0];
 
     if (!canDrawCurve && point) {
       this._strokeDraw(point);
@@ -207,8 +207,8 @@ export default class SignaturePad extends React.Component {
 
   _addPoint(point) {
     var points = this.points,
-        c2, c3,
-        curve, tmp;
+      c2, c3,
+      curve, tmp;
 
     points.push(point);
 
@@ -232,22 +232,22 @@ export default class SignaturePad extends React.Component {
 
   _calculateCurveControlPoints(s1, s2, s3) {
     var dx1 = s1.x - s2.x, dy1 = s1.y - s2.y,
-        dx2 = s2.x - s3.x, dy2 = s2.y - s3.y,
+      dx2 = s2.x - s3.x, dy2 = s2.y - s3.y,
 
-        m1 = {x: (s1.x + s2.x) / 2.0, y: (s1.y + s2.y) / 2.0},
-        m2 = {x: (s2.x + s3.x) / 2.0, y: (s2.y + s3.y) / 2.0},
+      m1 = { x: (s1.x + s2.x) / 2.0, y: (s1.y + s2.y) / 2.0 },
+      m2 = { x: (s2.x + s3.x) / 2.0, y: (s2.y + s3.y) / 2.0 },
 
-        l1 = Math.sqrt(dx1*dx1 + dy1*dy1),
-        l2 = Math.sqrt(dx2*dx2 + dy2*dy2),
+      l1 = Math.sqrt(dx1 * dx1 + dy1 * dy1),
+      l2 = Math.sqrt(dx2 * dx2 + dy2 * dy2),
 
-        dxm = (m1.x - m2.x),
-        dym = (m1.y - m2.y),
+      dxm = (m1.x - m2.x),
+      dym = (m1.y - m2.y),
 
-        k = l2 / (l1 + l2),
-        cm = {x: m2.x + dxm*k, y: m2.y + dym*k},
+      k = l2 / (l1 + l2),
+      cm = { x: m2.x + dxm * k, y: m2.y + dym * k },
 
-        tx = s2.x - cm.x,
-        ty = s2.y - cm.y;
+      tx = s2.x - cm.x,
+      ty = s2.y - cm.y;
 
     return {
       c1: new Point(m1.x + tx, m1.y + ty),
@@ -257,12 +257,12 @@ export default class SignaturePad extends React.Component {
 
   _addCurve(curve) {
     var startPoint = curve.startPoint,
-        endPoint = curve.endPoint,
-        velocity, newWidth;
+      endPoint = curve.endPoint,
+      velocity, newWidth;
 
     velocity = endPoint.velocityFrom(startPoint);
     velocity = this.velocityFilterWeight * velocity
-        + (1 - this.velocityFilterWeight) * this._lastVelocity;
+      + (1 - this.velocityFilterWeight) * this._lastVelocity;
 
     newWidth = this._strokeWidth(velocity);
     this._drawCurve(curve, this._lastWidth, newWidth);
@@ -281,38 +281,38 @@ export default class SignaturePad extends React.Component {
 
   _drawCurve(curve, startWidth, endWidth) {
     var ctx = this._ctx,
-        widthDelta = endWidth - startWidth,
-        drawSteps, width, i, t, tt, ttt, u, uu, uuu, x, y;
+      widthDelta = endWidth - startWidth,
+      drawSteps, width, i, t, tt, ttt, u, uu, uuu, x, y;
 
     drawSteps = Math.floor(curve.length());
     ctx.beginPath();
     for (i = 0; i < drawSteps; i++) {
-        // Calculate the Bezier (x, y) coordinate for this step.
-        t = i / drawSteps;
-        tt = t * t;
-        ttt = tt * t;
-        u = 1 - t;
-        uu = u * u;
-        uuu = uu * u;
+      // Calculate the Bezier (x, y) coordinate for this step.
+      t = i / drawSteps;
+      tt = t * t;
+      ttt = tt * t;
+      u = 1 - t;
+      uu = u * u;
+      uuu = uu * u;
 
-        x = uuu * curve.startPoint.x;
-        x += 3 * uu * t * curve.control1.x;
-        x += 3 * u * tt * curve.control2.x;
-        x += ttt * curve.endPoint.x;
+      x = uuu * curve.startPoint.x;
+      x += 3 * uu * t * curve.control1.x;
+      x += 3 * u * tt * curve.control2.x;
+      x += ttt * curve.endPoint.x;
 
-        y = uuu * curve.startPoint.y;
-        y += 3 * uu * t * curve.control1.y;
-        y += 3 * u * tt * curve.control2.y;
-        y += ttt * curve.endPoint.y;
+      y = uuu * curve.startPoint.y;
+      y += 3 * uu * t * curve.control1.y;
+      y += 3 * u * tt * curve.control2.y;
+      y += ttt * curve.endPoint.y;
 
-        width = startWidth + ttt * widthDelta;
-        this._drawPoint(x, y, width);
+      width = startWidth + ttt * widthDelta;
+      this._drawPoint(x, y, width);
     }
     ctx.closePath();
     ctx.fill();
   };
 
-  _strokeWidth (velocity) {
+  _strokeWidth(velocity) {
     return Math.max(this.maxWidth / (velocity + 1), this.minWidth);
   };
 
@@ -322,13 +322,12 @@ export default class SignaturePad extends React.Component {
         <div className="m-signature-pad--body">
           <canvas ref="cv"></canvas>
         </div>
-        { this.props.clearButton &&
+        {this.props.clearButton &&
           <div className="m-signature-pad--footer">
-            <button className="btn btn-default button clear" onClick={this.clear.bind(this)}>Clear</button>
+            <button className="btn btn-default button clear" onClick={this.clear.bind(this)}>{this.props.clearTxt || 'Clear'}</button>
           </div>
         }
       </div>
     );
   }
-
 }
